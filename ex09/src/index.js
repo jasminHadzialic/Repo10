ad0eb5a1ea4dd9e022887c0e7ea9ee5cff889eb0
jasminHadzialic <hadzialic.jasmin@gmail.com>
2021-09-23 00:07:00 +0200

    hasownproperty()

---
+++ ex09/src/index.js
@@ -0,0 +1,52 @@
+function myFunction(myObj, checkProp) {
+    // Only change code below this line
+    if (myObj.hasOwnProperty(checkProp)) {
+      return myObj[checkProp];
+    }
+    return "Not Found";
+  }
+  myFunction(
+    { title: "Titanic", song: "My Heart Will Go On", genre: "drama" },
+    "title"
+  );
+  myFunction(
+    { title: "Titanic", song: "My Heart Will Go On", genre: "drama" },
+    "song"
+  );
+  myFunction(
+    { title: "Titanic", song: "My Heart Will Go On", genre: "drama" },
+    "genre"
+  );
+  myFunction(
+    { title: "Titanic", song: "My Heart Will Go On", genre: "drama" },
+    "actor"
+  );
+  
+  console.log(
+    myFunction(
+      { title: "Titanic", song: "My Heart Will Go On", genre: "drama" },
+      "title"
+    )
+  );
+  console.log(
+    myFunction(
+      { title: "Titanic", song: "My Heart Will Go On", genre: "drama" },
+      "song"
+    )
+  );
+  console.log(
+    myFunction(
+      { title: "Titanic", song: "My Heart Will Go On", genre: "drama" },
+      "genre"
+    )
+  );
+  console.log(
+    myFunction(
+      { title: "Titanic", song: "My Heart Will Go On", genre: "drama" },
+      "actor"
+    )
+  );
+
+  
+  module.exports = myFunction;
+  
